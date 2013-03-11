697d957a0efbad230f96b5c78b6d4b15e99828d1
Joseph Smith <joe@uwcreations.com>
2013-03-11 16:58:16 -0500

    ci

--- project.clj
+++ project.clj
@@ -9,3 +9,4 @@
   :profiles {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.0-RC1"]]}})
+
